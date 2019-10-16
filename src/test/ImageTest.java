7608a91697f6f97a8a8ae30655e157390a7b5f49
ZYQ <1565223735@qq.com>
2019-10-16 22:55:14 +0800

    junit测试，从硬盘上读取图片

---
+++ src/test/ImageTest.java
@@ -0,0 +1,24 @@
+package test;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ImageTest {
+    @Test
+     void test(){
+
+        try {
+            BufferedImage image = ImageIO.read(new File("E:\\ideaWork\\tank\\src\\images\\bulletD.gif"));
+            assertNotNull(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // fail("1111");
+
+     }
+}
